1625c857819cb7cfcdfb2642fd6de10f45096f33
Francis Ribeiro <franedurib@gmail.com>
2018-02-15 18:51:01 -0200

    criação de novas abas

---
+++ src/screens/locacoes/aguardando.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Container } from 'native-base'
+
+export default class Aguardando extends Component {
+    render() {
+        return (
+            <Container style={{ backgroundColor: '#fff' }}>
+
+            </Container>
+        )
+    }
+}
---
+++ src/screens/locacoes/emAndamento.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Container } from 'native-base'
+
+export default class emAndamento extends Component {
+    render() {
+        return (
+            <Container style={{ backgroundColor: '#fff' }}>
+
+            </Container>
+        )
+    }
+}
